3335de06073d9dd633dd9d65a5db2fe3af83639c
Ujjwal Thaakar <ujjwalthaakar@gmail.com>
2015-05-10 16:15:47 +0530

    Fixed bugs in the code, restructered it and now add only url. Figured out that set difference does not work as expected on hash maps so now we do a set difference on sets of urls only

--- src/pocketsync/core.clj
+++ src/pocketsync/core.clj
@@ -17,11 +17,34 @@
 (def ronak-token (env :ronak-token))
 (def tag (env :tag))
 
-(defn -main
-  [& args]
-  (do
-    (pocketsync)
-    (set-interval pocketsync 3600000))) ; Sync every 1 hour
+(defn items
+  [token]
+  (set (map #(get % "given_url")
+         (-> (client/post retrieve-url
+                          {:form-params
+                           {:consumer_key consumer-key
+                            :access_token token
+                            :since since
+                            :tag tag}})
+             :body
+             json/read-str
+             (get "list")
+             vals))))
+
+(def add-url "https://getpocket.com/v3/add")
+
+(defn add-items
+  [token items]
+  (-> #(client/post add-url
+                   {:form-params
+                    {:consumer_key consumer-key
+                     :access_token token
+                     :url %
+                     :tags [tag]}})
+      (map items)))
+
+(defn set-interval [callback ms] 
+  (future (while true (do (Thread/sleep ms) (callback)))))
 
 (defn pocketsync
   []
@@ -33,32 +56,8 @@
     (add-items ronak-token for-ronak)
     (wcar* (car/set "since" (quot (System/currentTimeMillis) 1000)))))
 
-(defn items
-  [token]
-  (-> (client/post retrieve-url
-                   {:form-params
-                    {:consumer_key consumer-key
-                     :access_token token
-                     :since since
-                     :tag tag}})
-      :body
-      json/read-str
-      (get "list")
-      vals
-      set))
-
-(def add-url "https://getpocket.com/v3/add")
-
-(defn add-items
-  [token items]
-  (-> #(client/post add-url
-                   {:form-params
-                    {:consumer_key consumer-key
-                     :access_token token
-                     :url (get % "given_url")
-                     :tags [tag]}})
-      (map items)))
-
-(defn set-interval [callback ms] 
-  (future (while true (do (Thread/sleep ms) (callback)))))
-
+(defn -main
+  [& args]
+  (do
+    (pocketsync)
+    (set-interval pocketsync 3600000))) ; Sync every 1 hour
