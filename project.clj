5cb22f3b2b16ed75cc6005faaea422b16312d7f0
Ujjwal Thaakar <ujjwalthaakar@gmail.com>
2015-05-10 14:34:18 +0530

    Finished a synchronous first prototype of pocketsync

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [environ "1.0.0"]
+                 [clj-http "1.1.2"]
+                 [com.taoensso/carmine "2.10.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot pocketsync.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/pocketsync/core.clj
+++ src/pocketsync/core.clj
@@ -1,7 +1,64 @@
 (ns pocketsync.core
-  (:gen-class))
+  (:gen-class)
+  (:require [environ.core :refer [env]])
+  (:require [clj-http.client :as client])
+  (:require [taoensso.carmine :as car :refer [wcar]])
+  (:require [clojure.data.json :as json]))
+
+(def redis-conn {:pool nil :spec {:uri (env :rediscloud-url)}})
+(defmacro wcar*
+  [& body]
+  `(car/wcar redis-conn ~@body))
+
+(def since (wcar* (car/get "since")))
+(def retrieve-url "https://getpocket.com/v3/get")
+(def consumer-key (env :consumer-key))
+(def ujjwal-token (env :ujjwal-token))
+(def ronak-token (env :ronak-token))
+(def tag (env :tag))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (do
+    (pocketsync)
+    (set-interval pocketsync 3600000))) ; Sync every 1 hour
+
+(defn pocketsync
+  []
+  (let [ujjwal (items ujjwal-token)
+        ronak (items ronak-token)
+        for-ujjwal (clojure.set/difference ronak ujjwal)
+        for-ronak (clojure.set/difference ujjwal ronak)]
+    (add-items ujjwal-token for-ujjwal)
+    (add-items ronak-token for-ronak)
+    (wcar* (car/set "since" (quot (System/currentTimeMillis) 1000)))))
+
+(defn items
+  [token]
+  (-> (client/post retrieve-url
+                   {:form-params
+                    {:consumer_key consumer-key
+                     :access_token token
+                     :since since
+                     :tag tag}})
+      :body
+      json/read-str
+      (get "list")
+      vals
+      set))
+
+(def add-url "https://getpocket.com/v3/add")
+
+(defn add-items
+  [token items]
+  (-> #(client/post add-url
+                   {:form-params
+                    {:consumer_key consumer-key
+                     :access_token token
+                     :url (get % "given_url")
+                     :tags [tag]}})
+      (map items)))
+
+(defn set-interval [callback ms] 
+  (future (while true (do (Thread/sleep ms) (callback)))))
+
